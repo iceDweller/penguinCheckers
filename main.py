2781e946bbe24f3d796d451a19cefe89f4c61e8c
IceDweller <connor912@gmail.com>
2025-10-13 13:11:47 -0400

    pygame basic code

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock=pygame.time.Clock()
+running=True
+player_pos = pygame.mouse.get_pos()
+dt = 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+    screen.fill((0, 0, 0))
+    player_pos = pygame.mouse.get_pos()
+    pygame.draw.circle(screen, "red", player_pos, 10, 0)
+    pygame.display.flip()
+    dt = clock.tick(60) / 1000
+
+
+
 print("hello world")
 
 
