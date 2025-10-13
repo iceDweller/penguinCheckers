21dfa6df638c83bfced3546c6a01c909cbbf2b65
akapimbus <zacharylazurick@gmail.com>
2025-10-13 13:57:43 -0400

    adding base

--- main.py
+++ main.py
@@ -13,6 +13,23 @@ while running:
             running=False
     screen.fill((0, 0, 0))
     player_pos = pygame.mouse.get_pos()
+    col,row = 8,8
+    x,y = 0,0
+    while y < col*100:
+        while x < row*100:
+            pygame.draw.rect(screen, "blue", (x,y,100,100), 0)
+            pygame.draw.rect(screen, "green", (x+100,y,100,100), 0) 
+            x+=100
+        y+=100
+            
+
+    #pygame.draw.rect(screen, "green", (0,0,100,100), 0)
+    #pygame.draw.rect(screen, "blue", (100,0,100,100), 0)
+    #pygame.draw.rect(screen, "green", (200,0,100,100), 0)
+    #pygame.draw.rect(screen, "blue", (0,100,100,100), 0)
+    #pygame.draw.rect(screen, "green", (100,100,100,100), 0)
+    #pygame.draw.rect(screen, "blue", (200,200,100,100), 0)
+
     pygame.draw.circle(screen, "red", player_pos, 10, 0)
     pygame.display.flip()
     dt = clock.tick(60) / 1000
