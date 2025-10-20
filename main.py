da96a6483d63c35c70a5004b210a6f45f6798f4e
akapimbus <zacharylazurick@gmail.com>
2025-10-20 12:11:23 -0400

    madeboard

--- main.py
+++ main.py
@@ -18,24 +18,14 @@ while running:
     while x < row:
         while y < col:
             if (x+y) % 2 == 0:
-                pygame.draw.rect(screen, "green", (x*75,y*75,75,75), 0)
-                pygame.draw.circle(screen, "red", (x*75+37.5, y*75+37.5), 30, 0)
+                pygame.draw.rect(screen, "#dbc8a4", (x*75,y*75,75,75), 0)
             else:
-                pygame.draw.rect(screen, "blue", (x*75,y*75,75,75), 0)
+                pygame.draw.rect(screen, "#1c111c", (x*75,y*75,75,75), 0)
 
             y+=1
         x+=1
         y=0
-            
-            
-
-    #pygame.draw.rect(screen, "green", (0,0,100,100), 0)
-    #pygame.draw.rect(screen, "blue", (100,0,100,100), 0)
-    #pygame.draw.rect(screen, "green", (200,0,100,100), 0)
-    #pygame.draw.rect(screen, "blue", (0,100,100,100), 0)
-    #pygame.draw.rect(screen, "green", (100,100,100,100), 0)
-    #pygame.draw.rect(screen, "blue", (200,200,100,100), 0)
-
-    pygame.draw.circle(screen, "red", player_pos, 10, 0)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        pygame.draw.circle(screen, "red", player_pos, 10, 0)            
     pygame.display.flip()
     dt = clock.tick(60) / 1000
